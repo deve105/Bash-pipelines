1c79822e5b606c05bc7d72406a885a70e7f27128
Daniel Enriquez-Vera <daniel.enriquez.v@upch.pe>
2025-04-11 05:01:33 +0000

    ws

--- 2504_concatenate_mosdepth.sh
+++ 2504_concatenate_mosdepth.sh
@@ -7,6 +7,6 @@ echo "Version V1.3 2025-03-10"
 
 
 for file in *_.per-base.bed.gz; do
-    filename=$(basename "$file" _.per-base.bed.gz)  # Removes .gz extension
-    zcat "$file" | awk -v fname="$filename" '{print $0 "\t" fname}' >> ../2504_combined_output.tsv
+    filename=$(basename "$file" _.v2.regions.bed.gz)  # Removes .gz extension
+    zcat "$file" | awk -v fname="$filename" '{print $0 "\t" fname}' >> ../2504_regions_combined_output.tsv
 done
