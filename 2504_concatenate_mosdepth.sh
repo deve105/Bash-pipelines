0ba0fb61d731b9307ea53a78f9135376b3157140
Daniel Enriquez-Vera <daniel.enriquez.v@upch.pe>
2025-04-11 05:10:15 +0000

    ws

--- 2504_concatenate_mosdepth.sh
+++ 2504_concatenate_mosdepth.sh
@@ -6,7 +6,7 @@ echo "Script to concatenate txt files from HLA-LA output"
 echo "Version V1.3 2025-03-10"
 
 
-for file in *_v2.regions.bed.gz; do
+for file in .*_v2.regions.bed.gz; do
     filename=$(basename "$file" _v2.regions.bed.gz)  # Removes .gz extension
-    zcat "$file" | awk -v fname="$filename" '{print $0 "\t" fname}' >> ../2504_regions_combined_output.tsv
+    zcat "$file" | awk -v fname="$filename" '{print $0 "\t" fname}' >> ../2504_combined_output.tsv
 done
