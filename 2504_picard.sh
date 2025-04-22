37371234ee7889aaac68cfc78c6ed80a1e0fdb5f
Daniel Enriquez-Vera <daniel.enriquez.v@upch.pe>
2025-04-22 02:58:40 +0000

    ws

--- 2504_picard.sh
+++ 2504_picard.sh
@@ -70,8 +70,8 @@ while IFS= read -r sample_ID; do
     rm -rf ${proj_dir}/htlv1/${sample_ID}_htlv1_final.sorted.bam
     # Coverage analysis with mosdepth
     echo "Running mosdepth for coverage analysis"
-    mosdepth -t ${thrx} -n --by ${proj_dir}/J020209_1.fasta.bed \
-        ${proj_dir}/mosdepth/${sample_ID}_v4_ \
+    mosdepth -t ${thrx} -m --by ${proj_dir}/J020209_1.fasta.bed \
+        ${proj_dir}/mosdepth/${sample_ID}_v5_ \
         ${proj_dir}/htlv1/${sample_ID}_htlv1_marked.bam
         
     #${proj_dir}/J020209_1.fasta.bed -m --fast-mode 
