118a4cf62d1a0ae8852dc86d10ae84718465fc85
Daniel Enriquez-Vera <daniel.enriquez.v@upch.pe>
2025-07-19 02:40:41 +0000

    ws

--- 2507_virusbreak.sh
+++ 2507_virusbreak.sh
@@ -15,7 +15,7 @@ htlv_ref="/home/labatl/devapps/2407_references/2405project_hg38/J20209_1.fasta"
 # HTLV-1 reference name (adjust if using different HTLV)
 ref_genome="/home/labatl/devapps/2407_references/2405project_hg38/2405_hg38_bwa/2405_hg38htlv.fa"
 gridss_jar="/home/labatl/devapps/gridss/gridss.jar"
-virusbreakenddb="/home/labatl/Downloads/virusbreakenddb_20210401"
+virusbreakenddb="/home/labatl/Downloads/virusbreakenddb_20210401_new"
 listvirus="/home/labatl/devprojects/2507_breakend/htlv.txt"
 # Reading BAM files folder ending with _sorted.bam
 
@@ -26,7 +26,7 @@ while IFS= read -r sra; do
 
     # virus bam aligned
     virusbam="${outputdir}/${newname}_htlv_dedup.bam"
-    finaloutput="${outputdir}/output2/${newname}_breakend.vcf"
+    finaloutput="${outputdir}/output4/${newname}_breakend.vcf"
     
     # Run virusbreakend, continue to next file if it fails
     if ! virusbreakend \
