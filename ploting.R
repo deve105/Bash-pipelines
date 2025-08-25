7a739c79e9bc6cba1c070aa5d875d85875c3e76d
DEV-iOS <daniel.enriquez.v@upch.pe>
2025-08-25 10:16:19 +0900

    ws

--- ploting.R
+++ ploting.R
@@ -1,5 +1,80 @@
 pacman::p_load(tidyverse, tidyplots, zoo, signal)
 
+
+## To add HTLV-1 coverage to our dataset in google drive
+coverage_htlv = read_tsv("data/2504_mosdepth_global.tsv")
+htlvcov = coverage_htlv |>
+    dplyr::filter(chrom == "total") |>
+    dplyr::rename(sample = `22_SS01`) |>
+    dplyr::mutate(sample = stringr::str_remove(sample, "^[0-9]+_")) |>
+    dplyr::filter(!stringr::str_detect(sample, "ATL|1339")) |>
+    dplyr::select(mean, sample)
+
+## Reading the database to save it later
+htlv_db = googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/14eZCI8lRsfIebXu-tpAxbuPEOhE6moAnTDKKKUQyNOI/edit?gid=1412798579#gid=1412798579", sheet = "Sheet3")
+
+## Left joining
+newdb = htlv_db |>
+    left_join(htlvcov, by=c("ID2"="sample"))
+## Writing Sheet in Google Drive
+googlesheets4::write_sheet(newdb, ss="https://docs.google.com/spreadsheets/d/14eZCI8lRsfIebXu-tpAxbuPEOhE6moAnTDKKKUQyNOI/edit#gid=1412798579", sheet = "Sheet3")
+
+
+## Re-reading the dataset
+htlv_db = googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/14eZCI8lRsfIebXu-tpAxbuPEOhE6moAnTDKKKUQyNOI/edit?gid=1412798579#gid=1412798579", sheet = "Sheet3")
+
+
+## Calculating the correlation between PVl and meanCOV
+htlv_db = htlv_db |>
+    mutate(meanCOV=as.numeric(meanCOV),
+           PVL=as.numeric(PVL)) 
+tidyplots:: tidyplot(x=meanCOV, y=PVL) |>
+    tidyplots::add_data_points_jitter() |>
+    tidyplots::adjust_x_axis_title("Mean HTLV-1 Coverage", face="bold") |>
+    tidyplots::add_test_pvalue(test="spearman") |>
+    tidyplots::adjust_y_axis_title("Proviral Load (% PBMC)", face="bold") # |>
+    tidyplots::adjust_size(width = 8, height = 7, unit = "cm") #|>
+    tidyplots::save_plot(
+        filename = "2504_PVL_vs_MeanCOV.png",
+        background = "transparent",
+        dpi = 300
+    )
+
+
+labelx = paste("Spearman rho =", 
+                          round(cor(htlv_db$meanCOV, htlv_db$PVL, 
+                                    method="spearman", use="complete.obs"), 2),
+                          "\np = <2.2e-16") #, 
+                          #round(cor.test(htlv_db$meanCOV, htlv_db$PVL, 
+                                        #method="spearman")$p.value, 7))
+labelx
+cor.test(htlv_db$meanCOV, htlv_db$PVL, 
+                                        method="spearman")
+htlv_db |>
+    tidyplots::tidyplot(x=meanCOV, y=PVL, color=disease_2) |>
+    tidyplots::add_data_points_beeswarm(size=1.5, preserve="total", alpha=.8) |>
+    tidyplots::add(geom_smooth(
+      method = "lm", 
+      se = FALSE, 
+      color = "black", 
+      size=.5,
+      inherit.aes=FALSE, aes(x=meanCOV, y=PVL),
+      alpha = 0.4
+      )) |>
+    tidyplots::add_annotation_text(
+      labelx,
+      x = min(htlv_db$meanCOV, na.rm=TRUE) + 180,
+      y = max(htlv_db$PVL, na.rm=TRUE) - 1,
+    ) |>
+     tidyplots::adjust_x_axis_title("Mean HTLV-1 Coverage", face="bold") |>
+    tidyplots::adjust_y_axis_title("Proviral Load (% PBMC)", face="bold") |>
+    tidyplots::adjust_legend_title("Last Disease \nStatus") |>
+    tidyplots::adjust_colors(new_colors=c("#377EB8", "#E41A1C", "#a6611a")) |>
+    tidyplots::adjust_font(face="bold") |>
+    tidyplots::save_plot("output/2508_PVL_vs_Coverage.png", bg="transparent")
+
+
+
 global_cov = read_tsv("data/2504_mosdepth_global_perbase.tsv", col_names=FALSE) |>
     dplyr::rename(position=2, coverage=4, sample=5) |>
     dplyr::group_by(sample) |>
@@ -8,6 +83,7 @@ global_cov = read_tsv("data/2504_mosdepth_global_perbase.tsv", col_names=FALSE) 
     dplyr::ungroup(sample) |>
     dplyr::mutate(cohort=ifelse(str_detect(sample, "IRID|SS"), "Peru", "Miyazaki"))
 
+global_cov
 new_colors=c("#E69F00","#C02D45")
 global_cov |>
     tidyplot(x=position, y=smooth, color=cohort) |>
