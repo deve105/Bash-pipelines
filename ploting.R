585907454884db2afec782f8a7554cd16022dee9
DEV-iOS <daniel.enriquez.v@upch.pe>
2025-08-28 10:03:26 +0900

    ws

--- ploting.R
+++ ploting.R
@@ -23,21 +23,23 @@ googlesheets4::write_sheet(newdb, ss="https://docs.google.com/spreadsheets/d/14e
 ## Re-reading the dataset
 htlv_db = googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/14eZCI8lRsfIebXu-tpAxbuPEOhE6moAnTDKKKUQyNOI/edit?gid=1412798579#gid=1412798579", sheet = "Sheet3")
 
-
+htlv_db
 ## Calculating the correlation between PVl and meanCOV
 htlv_db = htlv_db |>
     mutate(meanCOV=as.numeric(meanCOV),
-           PVL=as.numeric(PVL)) 
-tidyplots:: tidyplot(x=meanCOV, y=PVL) |>
+           PVL=as.numeric(PVL2)) 
+
+htlv_db |>
+tidyplots:: tidyplot(x=meanCOV, y=PVL2) |>
     tidyplots::add_data_points_jitter() |>
     tidyplots::adjust_x_axis_title("Mean HTLV-1 Coverage", face="bold") |>
     tidyplots::add_test_pvalue(test="spearman") |>
     tidyplots::adjust_y_axis_title("Proviral Load (% PBMC)", face="bold") # |>
     tidyplots::adjust_size(width = 8, height = 7, unit = "cm") #|>
     tidyplots::save_plot(
-        filename = "2504_PVL_vs_MeanCOV.png",
+        filename = "2504_PVL_vs_MeanCOV.tiff",
         background = "transparent",
-        dpi = 300
+        dpi = 1200
     )
 
 
@@ -57,7 +59,7 @@ htlv_db |>
       method = "lm", 
       se = FALSE, 
       color = "black", 
-      size=.5,
+      linewidth=.5,
       inherit.aes=FALSE, aes(x=meanCOV, y=PVL),
       alpha = 0.4
       )) |>
@@ -71,7 +73,41 @@ htlv_db |>
     tidyplots::adjust_legend_title("Last Disease \nStatus") |>
     tidyplots::adjust_colors(new_colors=c("#377EB8", "#E41A1C", "#a6611a")) |>
     tidyplots::adjust_font(face="bold") |>
-    tidyplots::save_plot("output/2508_PVL_vs_Coverage.png", bg="transparent")
+    tidyplots::save_plot("output/2508_PVL_vs_Coverage.tiff", bg="transparent", dpi = 600)
+### PVL vs Gini 
+labely = paste("Spearman rho =", 
+                          round(cor(htlv_db$gini, htlv_db$PVL, 
+                                    method="spearman", use="complete.obs"), 2),
+                          "\np =",
+                          round(cor.test(htlv_db$gini, htlv_db$PVL, 
+                                        method="spearman")$p.value, 3))
+labely
+cor.test(htlv_db$meanCOV, htlv_db$PVL, 
+                                        method="spearman")
+
+htlv_db |>
+    tidyplots::tidyplot(x=gini, y=PVL, color=disease_2) |>
+    tidyplots::add_data_points_beeswarm(size=1.5, preserve="total", alpha=.8) |>
+    tidyplots::add(geom_smooth(
+      method = "lm", 
+      se = FALSE, 
+      color = "black", 
+      linewidth=.5,
+      inherit.aes=FALSE, aes(x=gini, y=PVL),
+      alpha = 0.4
+      )) |>
+    tidyplots::add_annotation_text(
+      labely,
+      x = min(htlv_db$gini, na.rm=TRUE) +0.3,
+      y = max(htlv_db$PVL, na.rm=TRUE) - 1,
+    ) |>
+     tidyplots::adjust_x_axis_title("Oligoclonality Index (OCI)", face="bold") |>
+    tidyplots::adjust_y_axis_title("Proviral Load (% PBMC)", face="bold") |>
+    tidyplots::adjust_legend_title("Last Disease \nStatus") |>
+    tidyplots::adjust_colors(new_colors=c("#377EB8", "#E41A1C", "#a6611a")) |>
+    tidyplots::adjust_font(face="bold") |>
+    tidyplots::save_plot("output/2508_PVL_vs_gini.tiff", bg="transparent", dpi = 600)
+
 
 
 
