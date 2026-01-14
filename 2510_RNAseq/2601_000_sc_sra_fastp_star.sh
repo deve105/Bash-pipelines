f45af46fa10ea254f369fd2053608dbd8c3263d8
DEV-Linux <daniel.enriquez.v@upch.pe>
2026-01-14 15:47:02 +0900

    qa

--- 2510_RNAseq/2601_000_sc_sra_fastp_star.sh
+++ 2510_RNAseq/2601_000_sc_sra_fastp_star.sh
@@ -6,23 +6,27 @@ echo "Script for SRA download, fastp QC, STAR mapping, and multiQC"
 echo "by Daniel Enriquez Version V1.2 2026-01-07"
 
 
+disc_tmp="/home/htlvatl/Documents/devapps/temp"
 hdd_tmp="/media/htlvatl/mem"
 filename=$(basename "$1")
 filename="${filename%.*}" # Removes the last extension
 filename_path="${hdd_tmp}/${filename}"
+filename_disc="${disc_tmp}/${filename}"
 platform="ILLUMINA"
 numthreads=16
 overhang=73
-
-
-
 
 # STAR index directory
 ref_dir="/home/htlvatl/coreapps/2025_refgenomes/2601_001_di_htlv1_ebv_hg38"
 merged_fasta="${ref_dir}/2601_001_rf_htlv1_ebv_hg38.fa"
 merged_gtf="${ref_dir}/2601_001_rf_htlv1_ebv_hg38.gtf"
+
+# STAR index specific to overhang
 index_dir="${ref_dir}/2601_001_rf_star_index_merged_h74"
 
+## ============================================================================
+## PRE-REQUISITES CHECKS - File arguments
+## ============================================================================
 #-If an argument is needed
 if [ $# -eq 0 ]; then
 	printf "Usage: %s project.txt is needed.\n" "$0"
@@ -41,6 +45,11 @@ if [ ! -s "$1" ]; then
 	exit 1
 fi
 
+
+## ============================================================================
+## PRE-REQUISITES CHECKS - Folder arguments
+## ============================================================================
+
 # Step 1 create project folder
 echo "Step 1. Create a folder for the project '$filename'"
 if [ -d "$filename_path" ]; then
@@ -51,12 +60,17 @@ else
 	mkdir -p "${filename_path}"/{rawdata,bam,logs,reports} #Creates the folder structure
 fi
 
-
-if [ ! -d "$index_dir" ]; then
-    printf "❌ Error: STAR index not found: %s\n" "$index_dir"
-    printf "   Please provide STAR index directory as second argument\n"
-    exit 1
+# Step 2 create temp project folder
+echo "Step 1. Create a Temporal folder for the project in HDD'$filename'"
+if [ -d "$filename_disc" ]; then
+	echo "Folder '$filename_disc' already exists. Be careful"
+	mkdir -p "${filename_disc}"/{rawdata,bam,logs,reports}
+else
+	echo "New folder '$filename_disc' created."
+	mkdir -p "${filename_disc}"/{rawdata,bam,logs,reports} #Creates the folder structure
 fi
+
+
 
 # ============================================================================
 # STEP 1 - CHECK DEPENDENCIES
@@ -73,13 +87,23 @@ done
 
 echo "✅ All dependencies found"
 
+
+# Step 3 - Check STAR index directory
+if [ ! -d "$index_dir" ]; then
+    printf "❌ Error: STAR index not found: %s\n" "$index_dir"
+    printf "   Please provide STAR index directory as second argument\n"
+    exit 1
+fi
+
 # ============================================================================
 # STEP 2 - PROCESS SRA IDS
 # ============================================================================
 
+
 # Step 3 - Download SRA data
 while IFS= read -r sra; do
-    cd "${hdd_tmp}"
+    cd "${disc_tmp}"
+    
     echo "Downloading ${sra}"
 
     # Step 3.1 - Prefetch the SRA data
@@ -92,41 +116,41 @@ while IFS= read -r sra; do
     if ! fasterq-dump --mem 30G \
                       --threads "${numthreads}" \
                       --skip-technical \
-                      --outdir "${filename_path}/rawdata/" \
-                      --temp "${filename_path}" \
+                      --outdir "${filename_disc}/rawdata/" \
+                      --temp "${filename_disc}" \
                       --split-3 \
-                      "${sra}"; then
+                      "${sra}" \
+                      2>&1 | tee -a "${filename_disc}/logs/${sra}_fasterq_dump.log"; then
         echo "Error: fasterq-dump failed for ${sra}"
+        rm -rf "${disc_tmp}/${sra}"
         exit 1
     fi
 
 		
 	# Check if the files are paired-end or single-end
-    if [ -f "${filename_path}/rawdata/${sra}_1.fastq" ] && [ -f "${filename_path}/rawdata/${sra}_2.fastq" ]; then
+    if [ -f "${filename_disc}/rawdata/${sra}_1.fastq" ] && [ -f "${filename_disc}/rawdata/${sra}_2.fastq" ]; then
         # Paired-end reads
-        echo "Paired-end reads detected for $sra"
-        mv "${filename_path}/rawdata/${sra}_1.fastq" "${filename_path}/rawdata/${sra}_1.fq"
-        mv "${filename_path}/rawdata/${sra}_2.fastq" "${filename_path}/rawdata/${sra}_2.fq"
-        gzip -f "${filename_path}/rawdata/${sra}_1.fq" "${filename_path}/rawdata/${sra}_2.fq"
-    elif [ -f "${filename_path}/rawdata/${sra}.fastq" ]; then
+        echo "Paired-end reads detected for ${sra}"
+        gzip -f "${filename_disc}/rawdata/${sra}_1.fastq" "${filename_disc}/rawdata/${sra}_2.fastq"
+    elif [ -f "${filename_disc}/rawdata/${sra}.fastq" ]; then
         # Single-end reads
         echo "Single-end reads detected for $sra"
-        mv "${filename_path}/rawdata/${sra}.fastq" "${filename_path}/rawdata/${sra}_1.fq"
-        gzip -f "${filename_path}/rawdata/${sra}_1.fq"
+        mv "${filename_disc}/rawdata/${sra}.fastq" "${filename_disc}/rawdata/${sra}_1.fastq"
+        gzip -f "${filename_disc}/rawdata/${sra}_1.fastq"
     else
         echo "Error: No valid FASTQ files found for $sra"
         exit 1
     fi
     echo "${sra} was downloaded and processed :)"
-    if [ -f "${filename_path}/rawdata/${sra}_1.fq.gz" ] && [ -f "${filename_path}/rawdata/${sra}_2.fq.gz" ]; then
+    if [ -f "${filename_disc}/rawdata/${sra}_1.fq.gz" ] && [ -f "${filename_disc}/rawdata/${sra}_2.fq.gz" ]; then
         echo "Performing fastp quality control for paired-end reads: ${sra}"
     # Paired-end mode
-        fastp -i "${filename_path}/rawdata/${sra}_1.fq.gz" \
-		    -I "${filename_path}/rawdata/${sra}_2.fq.gz" \
-            -o "${filename_path}/rawdata/${sra}_postqc_1.fq.gz" \
-            -O "${filename_path}/rawdata/${sra}_postqc_2.fq.gz" \
-			-j "${filename_path}/reports/${sra}_fastp.json" \
-            -h "${filename_path}/reports/${sra}_fastp.html" \
+        fastp -i "${filename_disc}/rawdata/${sra}_1.fastq.gz" \
+		    -I "${filename_disc}/rawdata/${sra}_2.fastq.gz" \
+            -o "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz" \
+            -O "${filename_disc}/rawdata/${sra}_postqc_2.fq.gz" \
+			-j "${filename_disc}/reports/${sra}_fastp.json" \
+            -h "${filename_disc}/reports/${sra}_fastp.html" \
             --detect_adapter_for_pe \
             --length_required 15 \
             --correction \
@@ -135,15 +159,15 @@ while IFS= read -r sra; do
             -w 12 \
             -u 40 \
             -n 5 \
-            2>&1 | tee -a "${filename_path}/logs/${sra}_fastp.log"
+            2>&1 | tee -a "${filename_disc}/logs/${sra}_fastp.log"
 
-    elif [ -f "${filename_path}/rawdata/${sra}_1.fq.gz" ]; then
+    elif [ -f "${filename_disc}/rawdata/${sra}_1.fastq.gz" ]; then
         echo "Performing fastp quality control for single-end reads: ${sra}"
         # Single-end mode
-        fastp -i "${filename_path}/rawdata/${sra}_1.fq.gz" \
-            -o "${filename_path}/rawdata/${sra}_postqc_1.fq.gz" \
-			-j "${filename_path}/reports/${sra}_fastp.json" \
-            -h "${filename_path}/reports/${sra}_fastp.html" \
+        fastp -i "${filename_disc}/rawdata/${sra}_1.fastq.gz" \
+            -o "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz" \
+			-j "${filename_disc}/reports/${sra}_fastp.json" \
+            -h "${filename_disc}/reports/${sra}_fastp.html" \
             --length_required 15 \
             --correction \
             -e 15 \
@@ -151,17 +175,18 @@ while IFS= read -r sra; do
             -w 12 \
             -u 40 \
             -n 5 \
-            2>&1 | tee -a "${filename_path}/logs/${sra}_fastp.log"
+            2>&1 | tee -a "${filename_disc}/logs/${sra}_fastp.log"
     else
-        echo "Error: No valid FASTQ files found for $sra"
+        echo "Error: No valid FASTQ files found for ${sra}"
         exit 1
     fi
-    # Step 3 - Download SRA data
-    if [ -f "${filename_path}/rawdata/${sra}_postqc_1.fq.gz" ] && [ -f "${filename_path}/rawdata/${sra}_postqc_2.fq.gz" ]; then
+    echo "Fastp QC completed for ${sra}"
+    # Step 3 - Mapping SRA 
+    if [ -f "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz" ] && [ -f "${filename_disc}/rawdata/${sra}_postqc_2.fq.gz" ]; then
         echo "Mapping for paired-end reads: ${sra}"
-        fastq1="${filename_path}/rawdata/${sra}_postqc_1.fq.gz"
-        fastq2="${filename_path}/rawdata/${sra}_postqc_2.fq.gz"
-        bamq="${filename_path}/bam/${sra}_postqc_"
+        fastq1="${filename_disc}/rawdata/${sra}_postqc_1.fq.gz"
+        fastq2="${filename_disc}/rawdata/${sra}_postqc_2.fq.gz"
+        bamq="${filename_disc}/bam/${sra}_postqc_"
         #MAPPING
         echo "STAR mapping for ${sra} for post-QC reads"
         # Paired-end alignment
@@ -201,18 +226,19 @@ while IFS= read -r sra; do
                 --alignInsertionFlush Right \
                 --alignSplicedMateMapLminOverLmate 0 \
                 --alignSplicedMateMapLmin 30 \
-             2>&1 | tee -a "${filename_path}/logs/${sra}_qc_star.log"
-        samtools index ${bamq}Aligned.sortedByCoord.out.bam
-        rm -f "${filename_path}/rawdata/${sra}_1.fq.gz"
-        rm -f "${filename_path}/rawdata/${sra}_2.fq.gz"
-        rm -f "${filename_path}/rawdata/${sra}_postqc_1.fq.gz"
-        rm -f "${filename_path}/rawdata/${sra}_postqc_2.fq.gz"
+             2>&1 | tee -a "${filename_disc}/logs/${sra}_qc_star.log"
+        samtools index "${bamq}Aligned.sortedByCoord.out.bam"
+        rm -f "${filename_disc}/rawdata/${sra}_1.fastq.gz"
+        rm -f "${filename_disc}/rawdata/${sra}_2.fastq.gz"
+        rm -f "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz"
+        rm -f "${filename_disc}/rawdata/${sra}_postqc_2.fq.gz"
+        rsync -av --remove-source-files "${bamq}"* "${filename_path}/bam/"
 
-    elif [ -f "${filename_path}/rawdata/${sra}_postqc_1.fq.gz" ]; then 
-    echo "Mapping for single-end reads: ${sra}"
-        fastq1="${filename_path}/rawdata/${sra}_postqc_1.fq.gz"
-        bamq="${filename_path}/bam/${sra}_postqc"
-    echo "STAR mapping for ${sra} for post-QC reads"
+    elif [ -f "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz" ]; then
+        echo "Mapping for single-end reads: ${sra}"
+        fastq1="${filename_disc}/rawdata/${sra}_postqc_1.fq.gz"
+        bamq="${filename_disc}/bam/${sra}_postqc"
+        echo "STAR mapping for ${sra} for post-QC reads"
         # Paired-end alignment
         STAR --twopassMode Basic \
                 --runThreadN "${numthreads}" \
@@ -250,10 +276,11 @@ while IFS= read -r sra; do
                 --alignInsertionFlush Right \
                 --alignSplicedMateMapLminOverLmate 0 \
                 --alignSplicedMateMapLmin 30 \
-             2>&1 | tee -a "${filename_path}/logs/${sra}_qc_star.log"
-        samtools index ${bamq}Aligned.sortedByCoord.out.bam
-        rm -f "${filename_path}/rawdata/${sra}_1.fq.gz"
-        rm -f "${filename_path}/rawdata/${sra}_postqc_1.fq.gz"
+             2>&1 | tee -a "${filename_disc}/logs/${sra}_qc_star.log"
+        samtools index "${bamq}Aligned.sortedByCoord.out.bam"
+        rm -f "${filename_disc}/rawdata/${sra}_1.fastq.gz"
+        rm -f "${filename_disc}/rawdata/${sra}_postqc_1.fq.gz"
+        rsync -av --remove-source-files "${bamq}"* "${filename_path}/bam/"
  
     fi
 
